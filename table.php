2ab8fed1c6820d5feaff23a063cfe9d21f22d69e
fahridika <96968769+fahridika@users.noreply.github.com>
2022-02-22 17:45:05 +0700

    Update table.php

--- table.php
+++ table.php
@@ -9,19 +9,20 @@
 <body>
     <table>
         <thead>
-            <!-- tr untuk membuat baris, td untuk membuat kolom, thead sebagai kepala -->
+            <!-- tr untuk membuat baris,
+             td untuk membuat kolom, thead sebagai kepala -->
         <tr>
-            <td> data 1, 2</td>
-            <td>data 1, 3</td>
-            <td>data 1, 4</td>
+            <td style="border: black solid 1px;"> data 1, 2</td>
+            <td style="border: black solid 1px;">data 1, 3</td>
+            <td style="border: black solid 1px;">data 1, 4</td>
         </tr>
         </thead>
         <!-- Tbody untuk menandai sebagai datanya atau bodynya -->
         <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+        <tr >
+            <td style="border: black solid 1px;" colspan="3"> data 2,1 data 2, 2 data 2, 2</td>
+            <td style="border: black solid 1px;"> data 2, 2</td>
+            <td style="border: black solid 1px;"> data 2, 3</td>
         </tr>
         </tbody>
         
