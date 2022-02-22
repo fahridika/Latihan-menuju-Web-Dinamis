6a69fb4970010acc5c3716777fc10fee5ba0b9fa
fahridika <96968769+fahridika@users.noreply.github.com>
2022-02-22 17:25:14 +0700

    Add files via upload
    membuat table doang wkwkwk

---
+++ table.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Table</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <!-- tr untuk membuat baris, td untuk membuat kolom, thead sebagai kepala -->
+        <tr>
+            <td> data 1, 2</td>
+            <td>data 1, 3</td>
+            <td>data 1, 4</td>
+        </tr>
+        </thead>
+        <!-- Tbody untuk menandai sebagai datanya atau bodynya -->
+        <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+        
+        
+    </table>
+    
+</body>
+</html>
